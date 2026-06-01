7aeae7e0781931945179a749b95ea9c55a60a592
shahin325-admin <shahin.safari1369@gmail.com>
2026-06-01 15:29:24 +0300

    added files

---
+++ expense-tracker/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="p-8">
+      <h1 className="text-4xl font-bold">Expense Tracker</h1>
+
+      <button className="mt-4 rounded bg-blue-500 px-4 py-2 text-white">
+        Add Expense
+      </button>
+    </div>
+  );
+}
+
+export default App;
